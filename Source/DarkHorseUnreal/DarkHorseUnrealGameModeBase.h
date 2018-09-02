24f50fa123f3d7c204dc4a0280d5dc7bd19bef05
Mohit Agrawal <mohit.agrawal2994@gmail.com>
2018-09-02 10:23:41 +0530

    Initial commit

---
+++ Source/DarkHorseUnreal/DarkHorseUnrealGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DarkHorseUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKHORSEUNREAL_API ADarkHorseUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
