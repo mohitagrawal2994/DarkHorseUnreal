a2d8e1fc68a55ceb9a067502f36d2c2712136f38
Mohit Agrawal <mohit.agrawal2994@gmail.com>
2018-09-20 15:42:13 +0530

    Creating the Character and Door Swing C++ Code
    Character C++ code has the mobility setup
    
    Door Swing provide the door mobility for opening/closing it when the character presses E

---
+++ Source/DarkHorseUnreal/DoorSwing.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DoorSwing.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+
+// Sets default values
+ADoorSwing::ADoorSwing()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	DoorFrame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorFrame"));		//Assigning a static mesh component
+	RootComponent = DoorFrame;
+	DoorFrame->SetMobility(EComponentMobility::Static);									//Setting its mobility static
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));					//Assigning a movable mesh component
+	Door->SetupAttachment(RootComponent);
+
+	PressurePlate = CreateDefaultSubobject<UBoxComponent>(TEXT("PressurePlate"));		//Assigning a static box component
+	PressurePlate->SetupAttachment(RootComponent);
+	PressurePlate->SetCollisionProfileName("Trigger");									//Setting its collision preset to be trigger
+	PressurePlate->SetMobility(EComponentMobility::Static);								//Setting its mobility static
+	PressurePlate->InitBoxExtent(FVector(60, 200, 160));								//Setting the box component size
+	PressurePlate->SetWorldTransform(FTransform(FVector(53.35, 0, 160)));				//Setting the box component transform
+
+	Closing = false;
+	Opening = false;
+	isClosed = true;
+	LSwing = true;
+
+	MaxDegree = 0.0f;
+	AddRotation = 0.0f;
+	DoorCurrentRotation = 0.0f;
+
+
+}
+
+// Called when the game starts or when spawned
+void ADoorSwing::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoorSwing::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (Opening)
+	{
+		OpenDoor(DeltaTime);
+	}
+
+	if (Closing)
+	{
+		CloseDoor(DeltaTime);
+	}
+
+}
+
+void ADoorSwing::OpenDoor(float dt)
+{
+	if (LSwing)
+	{
+		AddRotation = -dt * 150;
+		MaxDegree = -90.0f;
+	}
+	else
+	{
+		AddRotation = dt * 150;
+		MaxDegree = -90.0f;
+	}
+	DoorCurrentRotation = Door->RelativeRotation.Yaw;
+	if (FMath::IsNearlyEqual(DoorCurrentRotation, MaxDegree, 1.5f))
+	{
+		Closing = false;
+		Opening = false;
+	}
+	else if (Opening)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Opening Rotation is %f"), AddRotation);
+		FRotator NewRotation = FRotator(0.0f, AddRotation, 0.0f);
+		Door->AddRelativeRotation(FQuat(NewRotation));
+	}
+}
+
+void ADoorSwing::CloseDoor(float dt)
+{
+	if (LSwing)
+	{
+		AddRotation = dt * 150;
+		MaxDegree = 0;
+	}
+	else
+	{
+		AddRotation = -dt * 150;
+		MaxDegree = -180;
+	}
+	DoorCurrentRotation = Door->RelativeRotation.Yaw;
+	if (FMath::IsNearlyEqual(DoorCurrentRotation, MaxDegree, 1.5f))
+	{
+		Closing = false;
+		Opening = false;
+	}
+	else if (Closing)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Closing Rotation is %f"), AddRotation);
+		FRotator NewRotation = FRotator(0.0f, AddRotation, 0.0f);
+		Door->AddRelativeRotation(FQuat(NewRotation));
+	}
+}
+
+void ADoorSwing::ToggleDoor()
+{
+	//UE_LOG(LogTemp, Warning, TEXT("Door is Toggled with Dot P %f"),DotP);
+	if (isClosed)
+	{
+		isClosed = false;
+		Closing = false;
+		Opening = true;
+	}
+	else
+	{
+		isClosed = true;
+		Opening = false;
+		Closing = true;
+	}
+}
+
---
+++ Source/DarkHorseUnreal/DoorSwing.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DoorSwing.generated.h"
+
+UCLASS()
+class DARKHORSEUNREAL_API ADoorSwing : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleDefaultsOnly)					//Creating a pointer of type static mesh
+	class UStaticMeshComponent* DoorFrame;
+
+	UPROPERTY(VisibleDefaultsOnly)					//Creating a pointer of type static mesh
+	class UStaticMeshComponent* Door;
+
+	UPROPERTY(VisibleDefaultsOnly)					//Creating a pointer of type box component
+	class UBoxComponent* PressurePlate;
+
+	bool Opening;
+	bool Closing;
+	bool isClosed;									//Is set to false if the door is open
+
+	float MaxDegree;
+	float AddRotation;
+	float DoorCurrentRotation;
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoorSwing();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OpenDoor(float dt);
+
+	UFUNCTION()
+	void CloseDoor(float dt);
+
+	UFUNCTION()
+	void ToggleDoor();
+
+	UPROPERTY(EditDefaultsOnly)
+	bool LSwing;
+
+};
