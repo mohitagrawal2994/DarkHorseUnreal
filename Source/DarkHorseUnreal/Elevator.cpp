1ceced784eb8b9952b6d176f4e243c84ff9121f2
Mohit Agrawal <mohit.agrawal2994@gmail.com>
2018-11-08 23:50:00 +0530

    Creating the variables to store and hold mesh components in elevator actor class

--- Source/DarkHorseUnreal/Elevator.cpp
+++ Source/DarkHorseUnreal/Elevator.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Elevator.h"
-
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AElevator::AElevator()
@@ -9,6 +9,75 @@ AElevator::AElevator()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ElevatorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ElevatorMesh"));
+	RootComponent = ElevatorMesh;
+	ElevatorMesh->SetMobility(EComponentMobility::Movable);
+
+	Bar1Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bar1"));
+	Bar1Mesh->SetupAttachment(RootComponent);
+	Bar1Mesh->SetWorldTransform(FTransform(FVector(101.5, -5, 102)));				//Setting the mesh component transform
+
+	Bar2Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bar2"));
+	Bar2Mesh->SetupAttachment(RootComponent);
+	Bar2Mesh->SetWorldTransform(FTransform(FVector(-101.5, -5, 102)));				//Setting the mesh component transform
+	Bar2Mesh->SetWorldRotation(FRotator(0.0f, 180.0f, 0.0f));
+
+	Door1Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door1"));
+	Door1Mesh->SetupAttachment(RootComponent);
+	Door1Mesh->SetWorldTransform(FTransform(FVector(0, 117, 12.7)));				//Setting the mesh component transform
+
+	Door2Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door2"));
+	Door2Mesh->SetupAttachment(RootComponent);
+	Door2Mesh->SetWorldTransform(FTransform(FVector(0, 122.08, 12.7)));				//Setting the mesh component transform
+	Door2Mesh->SetWorldRotation(FRotator(0.0f, 180.0f, 0.0f));
+																				
+	Button1Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button1"));	//Setting up the buttons
+	Button1Mesh->SetupAttachment(RootComponent);
+	Button1Mesh->SetWorldTransform(FTransform(FVector(82.8, 115.8, 148.)));
+	Button1Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button1Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+
+	Button2Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button2"));
+	Button2Mesh->SetupAttachment(RootComponent);
+	Button2Mesh->SetWorldTransform(FTransform(FVector(75, 115.8, 148)));
+	Button2Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button2Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+
+	Button3Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button3"));
+	Button3Mesh->SetupAttachment(RootComponent);
+	Button3Mesh->SetWorldTransform(FTransform(FVector(82.8, 115.8, 142.5)));
+	Button3Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button3Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+
+	Button4Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button4"));
+	Button4Mesh->SetupAttachment(RootComponent);
+	Button4Mesh->SetWorldTransform(FTransform(FVector(75, 115.8, 142.5)));
+	Button4Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button4Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+
+	Button5Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button5"));
+	Button5Mesh->SetupAttachment(RootComponent);
+	Button5Mesh->SetWorldTransform(FTransform(FVector(82.8, 115.8, 137)));
+	Button5Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button5Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+
+	Button6Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button6"));
+	Button6Mesh->SetupAttachment(RootComponent);
+	Button6Mesh->SetWorldTransform(FTransform(FVector(75, 115.8, 137)));
+	Button6Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button6Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+
+	Button7Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button7"));
+	Button7Mesh->SetupAttachment(RootComponent);
+	Button7Mesh->SetWorldTransform(FTransform(FVector(82.8, 115.8, 131.5)));
+	Button7Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button7Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+
+	Button8Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button8"));
+	Button8Mesh->SetupAttachment(RootComponent);
+	Button8Mesh->SetWorldTransform(FTransform(FVector(75, 115.8, 131.5)));
+	Button8Mesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	Button8Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
 }
 
 // Called when the game starts or when spawned
--- Source/DarkHorseUnreal/Elevator.h
+++ Source/DarkHorseUnreal/Elevator.h
@@ -10,19 +10,55 @@ UCLASS()
 class DARKHORSEUNREAL_API AElevator : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AElevator();
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* ElevatorMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Bar1Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Bar2Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Door1Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Door2Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button1Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button2Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button3Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button4Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button5Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button6Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button7Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class UStaticMeshComponent* Button8Mesh;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
+	// Sets default values for this actor's properties
+	AElevator();
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
-	
 };
